12cbb2b3cf167a3f0b774f72296a8607eaee50ce
jeikerxiao <jeiker@126.com>
2019-12-19 13:09:15 +0800

    add redis config

---
+++ spring-boot-redis1/src/main/java/com/jeiker/redis/config/RedisConfig.java
@@ -0,0 +1,36 @@
+package com.jeiker.redis.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * Description: Redis 缓存配置
+ * User: jeikerxiao
+ * Date: 2019-12-19 12:56
+ */
+// 配置
+@Configuration
+// 开启缓存
+@EnableCaching
+public class RedisConfig extends CachingConfigurerSupport {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory connectionFactory) {
+        // 配置redisTemplate
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+        // key序列化
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        // value序列化
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
+}
--- spring-boot-redis1/src/test/java/com/jeiker/redis/SpringBootRedisApplicationTests.java
+++ spring-boot-redis1/src/test/java/com/jeiker/redis/SpringBootRedisApplicationTests.java
@@ -1,13 +1,26 @@
 package com.jeiker.redis;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
 class SpringBootRedisApplicationTests {
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @Test
-    void contextLoads() {
+    public void set() {
+        redisTemplate.opsForValue().set("test:set1", "testValue1");
+        Object object = redisTemplate.opsForValue().get("test:set1");
+        System.out.println(object.toString());
+
     }
+
 
 }
